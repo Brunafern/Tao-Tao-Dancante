7a032c9ca9f7bb83e9297d934c773854a9951879
CamilaDornelas <cmmdornelas@gmail.com>
2025-08-17 19:03:05 -0300

    Adicionando as interfaces

--- src/main/java/jogo/interfaces/PauseInterface.java
+++ src/main/java/jogo/interfaces/PauseInterface.java
@@ -6,6 +6,6 @@ public interface Pause {
     void pause() throws jogo.excecoes.RecursoException;
     void voltar();
     boolean estaPausado();
-        void voltarParaMenu(ActionEvent event) throws jogo.excecoes.RecursoException, jogo.excecoes.FluxoException;
+    void voltarParaMenu(ActionEvent event) throws jogo.excecoes.RecursoException, jogo.excecoes.FluxoException;
     void sairDoJogo() throws jogo.excecoes.RecursoException, jogo.excecoes.FluxoException;
 }
